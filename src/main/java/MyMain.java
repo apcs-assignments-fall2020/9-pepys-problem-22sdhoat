4d97ccf699a9e7ee586f89be7a6a7837459ea3c9
22sdhoat <22sdhoat@athenian.org>
2020-10-03 21:29:40 +0000

    finding the probability of rolling a 6 out of 6,12, or 18 times

--- src/main/java/MyMain.java
+++ src/main/java/MyMain.java
@@ -5,26 +5,64 @@ public class MyMain {
     // Returns in the answer as a double corresponding to the percentage
     // For example, 75.5% would be 75.5
     public static double probabilityOneSix() {
-        // REPLACE WITH YOUR CODE HERE
-        return -1.0;
+        int trials = 10000;
+        int exp = 0;
+        for (int i = 0; i < trials; i++) {
+            boolean success = false;
+            for (int roll = 0; roll < 6; roll++){
+                if ((int)(Math.random() * 6) + 1 == 1) {
+                    success = true;
+                }
+            }
+            if (success) {
+                exp++;
+            }
+        }
+        return (double)exp/100;
     }
 
     // Calculate the probability of rolling at least two 6's when rolling 
     // twelve dice. Uses 10000 trials.
     public static double probabilityTwoSixes() {
-        // REPLACE WITH YOUR CODE HERE
-        return -1.0;
+        int trials = 10000;
+        int exp = 0;
+        for (int i = 0; i < trials; i++){
+            int sum = 0;
+            for (int roll = 0; roll < 12; roll++){
+                if ((int)(Math.random() * 6) + 1 == 1){
+                    sum++;
+                }
+            }
+            if (sum >= 2){
+                exp++;
+            }
+        }
+        return (double)exp/100;
     }
 
     // Calculate the probability of rolling at least three 6's when rolling 
     // eighteen dice. Uses 10000 trials.
     public static double probabilityThreeSixes() {
-        // REPLACE WITH YOUR CODE HERE
-        return -1.0;
+        int trials = 10000;
+        int exp = 0;
+        for (int i = 0; i < trials; i++){
+            int sum = 0;
+            for (int roll = 0; roll < 18; roll++){
+                if ((int)(Math.random() * 6) + 1 == 1){
+                    sum++;
+                }
+            }
+            if (sum >= 3){
+                exp++;
+            }
+        }
+        return (double)exp/100;
     }
 
 
     public static void main(String[] args) {
-        // YOUR CODE HERE
+        System.out.println(probabilityOneSix());
+        System.out.println(probabilityTwoSixes());
+        System.out.println(probabilityThreeSixes());
     }
 }
